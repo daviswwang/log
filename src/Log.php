6b4c945d0f1378378ee4ce946c40af6b4407673d
davis <130086230@qq.com>
2021-04-12 16:48:59 +0800

    up

--- src/Log.php
+++ src/Log.php
@@ -15,22 +15,11 @@ use Hyperf\Utils\Context;
 class Log
 {
 
-    public $starTime;
-    public $starMemory;
-    public $fileName = null;
-
-
-    public function __construct()
+    public function add_debug(array $debug)
     {
-        $this->starTime = microtime(true);
-        $this->starMemory = memory_get_usage();
-    }
-
-    public function add_debug(array $params)
-    {
-        $debug = Context::get('add_debug') ?: [];
-        array_push($debug, $params);
         Context::set('add_debug', $debug);
+        Context::set('starTime', microtime(true));
+        Context::set('starMemory', memory_get_usage());
     }
 
     public function add_mysqlDebug(array $params)
@@ -49,7 +38,9 @@ class Log
             mkdir($filePath, 755, true);
         }
 
-        $this->fileName = $filePath . '/' . $userId . '_' . date('H-i-s') . '_' . mt_rand(100, 999) . '.md';
+        $fileName = $filePath . '/' . $userId . '_' . date('H-i-s') . '_' . mt_rand(100, 999) . '.md';
+
+        Context::set('fileName', $fileName);
     }
 
 
@@ -62,39 +53,42 @@ class Log
     {
         $debug = Context::get('add_debug');
         $mysqlDebug = Context::get('add_mysqlDebug');
+        $starTime = Context::get('starTime');
+        $starMemory = Context::get('starMemory');
+        $fileName = Context::get('fileName');
 
-        if (!$this->fileName || !$debug) return;
+        if (!$fileName || !$debug) return;
 
         $data = [];
         $data[] = "## 请求数据\n";
         $data[] = " - METHOD:\t\t{$request->getMethod()}\n";
         $data[] = " - GET_URL:\t\t{$request->getUri()}\n";
-        $data[] = " - SERV_IP:\t\t" . ($_SERVER['SERVER_ADDR'] ?? '') . "\n";
-        $data[] = " - USER_IP:\t\t" . ($_SERVER['REMOTE_ADDR'] ?? '') . "\n";
-        $data[] = " - REAL_IP:\t\t" . ($_SERVER['X-REAL-IP'] ?? '') . "\n";
+//        $data[] = " - SERV_IP:\t\t" . ($_SERVER['SERVER_ADDR'] ?? '') . "\n";
+//        $data[] = " - USER_IP:\t\t" . ($_SERVER['REMOTE_ADDR'] ?? '') . "\n";
+//        $data[] = " - REAL_IP:\t\t" . ($_SERVER['X-REAL-IP'] ?? '') . "\n";
         $data[] = " - DATETIME:\t" . date('Y-m-d H:i:s') . "\n";
-        $data[] = " - AGENT:\t\t" . ($_SERVER['HTTP_USER_AGENT'] ?? '') . "\n";
-        $data[] = " - Router:\t\t{$request->getPathInfo()}\n";
+//        $data[] = " - AGENT:\t\t" . ($_SERVER['HTTP_USER_AGENT'] ?? '') . "\n";
+        $data[] = " - Router:\t\t{$request->fullUrl()}\n";
 
         $data[] = " \n\n\n\n";
         //一些路由结果，路由结果参数
 
-        $data[] = "## 路由参数\n";
-        $Params = $request->getHeaders();
-        $data[] = " - Header请求参数:\t\t\n";
-        if (!empty($Params)) {
-            foreach ($Params as $k => $v) {
-                if (is_array($v)) {
-                    $data[] = " \t\t- {$k}中参数:\t\t\n";
-                    foreach ($v as $kk => $item) {
-                        if (is_array($item)) $item = json_encode($item, JSON_UNESCAPED_UNICODE);
-                        $data[] = "\t\t\t\t- {$kk}\t{$item}\n";
-                    }
-                } else {
-                    $data[] = "- {$k}\t{$v}\n";
-                }
-            }
-        }
+//        $data[] = "## 路由参数\n";
+//        $Params = $request->getHeaders();
+//        $data[] = " - Header请求参数:\t\t\n";
+//        if (!empty($Params)) {
+//            foreach ($Params as $k => $v) {
+//                if (is_array($v)) {
+//                    $data[] = " \t\t- {$k}中参数:\t\t\n";
+//                    foreach ($v as $kk => $item) {
+//                        if (is_array($item)) $item = json_encode($item, JSON_UNESCAPED_UNICODE);
+//                        $data[] = "\t\t\t\t- {$kk}\t{$item}\n";
+//                    }
+//                } else {
+//                    $data[] = "- {$k}\t{$v}\n";
+//                }
+//            }
+//        }
 
         $data[] = "## 路由参数\n";
         $Params = $request->all();
@@ -115,8 +109,8 @@ class Log
 
         //执行时间 内存消耗
         $data[] = "## 执行时间 内存消耗\n```\n";
-        $time = sprintf('% 9.3f', (microtime(true) - $this->starTime) * 1000);
-        $memo = sprintf('% 9.3f', (memory_get_usage() - $this->starMemory) / 1024);
+        $time = sprintf('% 9.3f', (microtime(true) - $starTime) * 1000);
+        $memo = sprintf('% 9.3f', (memory_get_usage() - $starMemory) / 1024);
         $total = sprintf('% 9.3f', (memory_get_usage()) / 1024);
         $data[] = "\t\tuTime\tuMem\t\ttMem\t\n";
         $data[] = "  {$time}\t{$memo}\t{$total}\t\n```\n";
@@ -178,6 +172,6 @@ class Log
 
         $data[] = "\n";
 
-        file_put_contents($this->fileName, $data, LOCK_EX);
+        file_put_contents($fileName, $data, LOCK_EX);
     }
 }
