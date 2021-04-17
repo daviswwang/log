ce0ed45a3a414a7f4da8ba155339ea92a9ae9024
davis <130086230@qq.com>
2021-04-17 16:36:03 +0800

    up

--- src/Log.php
+++ src/Log.php
@@ -43,9 +43,8 @@ class Log
         Context::set('fileName', $fileName);
     }
 
-    public function start_debug($debug, $shopCode, $userId)
+    public function start_debug($shopCode, $userId)
     {
-        Context::set('add_debug', $debug);
         Context::set('starTime', microtime(true));
         Context::set('starMemory', memory_get_usage());
 
@@ -68,13 +67,12 @@ class Log
      */
     public function save_log($request, $response = [])
     {
-        $debug = Context::get('add_debug');
         $mysqlDebug = Context::get('add_mysqlDebug');
         $starTime = Context::get('starTime');
         $starMemory = Context::get('starMemory');
         $fileName = Context::get('fileName');
 
-        if (!$fileName || !$debug) return;
+        if (!$fileName) return;
 
         $data = [];
         $data[] = "## 请求数据\n";
@@ -88,24 +86,6 @@ class Log
         $data[] = " - Router:\t\t{$request->fullUrl()}\n";
 
         $data[] = " \n\n\n\n";
-        //一些路由结果，路由结果参数
-
-//        $data[] = "## 路由参数\n";
-//        $Params = $request->getHeaders();
-//        $data[] = " - Header请求参数:\t\t\n";
-//        if (!empty($Params)) {
-//            foreach ($Params as $k => $v) {
-//                if (is_array($v)) {
-//                    $data[] = " \t\t- {$k}中参数:\t\t\n";
-//                    foreach ($v as $kk => $item) {
-//                        if (is_array($item)) $item = json_encode($item, JSON_UNESCAPED_UNICODE);
-//                        $data[] = "\t\t\t\t- {$kk}\t{$item}\n";
-//                    }
-//                } else {
-//                    $data[] = "- {$k}\t{$v}\n";
-//                }
-//            }
-//        }
 
         $data[] = "## 路由参数\n";
         $Params = $request->getParsedBody();
@@ -135,7 +115,6 @@ class Log
         $data[] = "  {$time}\t{$memo}\t{$total}\t\n```\n";
 
         if (is_array($mysqlDebug) && count($mysqlDebug)) {
-            $slow = [];
             $data[] = "\n## Mysql 顺序：\n";
             $data[] = " - 当前共执行MYSQL：\t" . count($mysqlDebug) . " 次\n";
             foreach ($mysqlDebug as $i => $value) {
@@ -144,27 +123,6 @@ class Log
                 if (isset($value['parmars'])) $data[] = "\t\t执行结果\t :" . json_encode($value['parmars']) . "\n";
                 $data[] = "\n";
             }
-        }
-
-        //程序执行顺序
-        $data[] = "## 程序执行顺序\n```\n";
-
-        foreach ($debug as $value) {
-            $data[] = "\t\t文件位置:" . ($value[0] ?? '') . "\n";
-            $data[] = "\t\t行数:" . ($value[1] ?? '') . "\n";
-            $data[] = "\t\t描述:" . ($value[2] ?? '') . "\n";
-            $data[] = "\t\t详情:\n";
-            $value3 = $value[3] ?? '';
-
-            if (is_array($value3) || is_object($value3)) {
-                $value3 = json_decode(json_encode($value3), true);
-
-                foreach ($value3 as $k => $v) {
-                    if (is_array($v)) $v = json_encode($v, JSON_UNESCAPED_UNICODE);
-                    $data[] = " \t\t\t\t-{$k}\t{$v}\n";
-                }
-            }
-            $data[] = "\n";
         }
 
         if (!empty($response)) {
